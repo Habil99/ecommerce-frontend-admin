d4aad86fccf709f3205ba04a3f9a1a41206545d0
Habil99 <abiyevhabil3@gmail.com>
2023-10-30 22:25:40 +0400

    chore: revert eslint config because of several issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'airbnb-typescript/base',
     'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:import/typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
