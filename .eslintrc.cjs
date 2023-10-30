bce21674b2fc7a497215ee750dbb4c4279b25d0b
Habil99 <abiyevhabil3@gmail.com>
2023-10-30 22:10:55 +0400

    feat(eslint-prettier): setup airbnb eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb-typescript/base',
+    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
